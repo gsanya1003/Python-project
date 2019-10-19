9e783e59282e7ddb47c0ccc6c8b70349dfb1d677
007atulpandey <46052653+007atulpandey@users.noreply.github.com>
2019-10-19 22:21:05 +0530

    Update samyu.py

--- samyu.py
+++ samyu.py
@@ -1,1 +1,7 @@
-print("Hello ,Sanya")
+#Essentially, in Python 3, the print statement has been replaced with a print () function.
+#For example, in Python 2 it is print “hello” but in Python 3 it is print (“hello”)
+
+int a
+print("this is a string ")
+print(a)
+#a is a variable 
